5bb0e8f6c5c63ab6e5d36bfec10aa2e06b05a00f
JuliansRecent <juliansrecent@gmail.com>
2023-01-31 19:00:37 -0800

    Module complete. Create a terminal based application that takes two strings form the user and prints a statement saying if they are the same or different.

--- src/main/java/Same.java
+++ src/main/java/Same.java
@@ -6,6 +6,18 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        // Write your program here.
+        // Prompt the user for two strings
+        System.out.println("Enter the first string:");
+        String string1 = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String string2 = scan.nextLine();
+
+        // Compare the strings and tell the user if they are the same or different
+        if (string1.equals(string2)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
